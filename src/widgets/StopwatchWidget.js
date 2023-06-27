186461ee11a89ca300ee0ad5ffa5fb2d9f514c65
abheyy <iamabhaydhasmana@gmail.com>
2023-06-27 10:44:30 +0530

    a

---
+++ src/widgets/StopwatchWidget.js
@@ -0,0 +1,46 @@
+import React, { useState, useRef } from 'react';
+
+const StopwatchWidget = () => {
+  const [isRunning, setIsRunning] = useState(false);
+  const [elapsedTime, setElapsedTime] = useState(0);
+  const intervalRef = useRef(null);
+
+  const startStopwatch = () => {
+    if (isRunning) {
+      clearInterval(intervalRef.current);
+    } else {
+      const startTime = Date.now() - elapsedTime;
+      intervalRef.current = setInterval(() => {
+        setElapsedTime(Date.now() - startTime);
+      }, 10);
+    }
+    setIsRunning(!isRunning);
+  };
+
+  const resetStopwatch = () => {
+    clearInterval(intervalRef.current);
+    setElapsedTime(0);
+    setIsRunning(false);
+  };
+
+  const formatTime = (time) => {
+    const milliseconds = `0${Math.floor(time / 10) % 100}`.slice(-2);
+    const seconds = `0${Math.floor(time / 1000) % 60}`.slice(-2);
+    const minutes = `0${Math.floor(time / 60000) % 60}`.slice(-2);
+    const hours = `0${Math.floor(time / 3600000)}`.slice(-2);
+    return `${hours}:${minutes}:${seconds}.${milliseconds}`;
+  };
+
+  return (
+    <div style={{ minWidth: 300 }}>
+      <p>Stopwatch </p>
+      <div>
+        <p>{formatTime(elapsedTime)}</p>
+        <button onClick={startStopwatch}>{isRunning ? 'Stop' : 'Start'}</button>
+        <button onClick={resetStopwatch}>Reset</button>
+      </div>
+    </div>
+  );
+};
+
+export default StopwatchWidget;
---
+++ src/widgets/WeatherWidget.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+
+const WeatherWidget = () => {
+  const [weatherData, setWeatherData] = useState(null);
+  const API_KEY = '7c50d6127c6ecbee83c5a0a0ef1be3c2'; // Replace with your actual API key
+  const location = 'Mumbai'; // Replace with the desired location
+
+  useEffect(() => {
+    fetchWeatherData();
+  }, []);
+
+  const fetchWeatherData = async () => {
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`
+      );
+      const data = await response.json();
+      setWeatherData(data);
+    } catch (error) {
+      console.log('Error fetching weather data:', error);
+    }
+  };
+
+  return (
+    <div style={{ minWidth: 300 }}>
+      <p>Weather</p>
+      {weatherData ? (
+        <div>
+          <p>Location: {weatherData.name}</p>
+          <p>Temperature: {weatherData.main.temp}°C</p>
+          <p>Humidity: {weatherData.main.humidity}%</p>
+          <p>Weather: {weatherData.weather[0].description}</p>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
+
+export default WeatherWidget;
